90082012f3dada00433f99aff87c82b34c4dd3d1
unknown <nellysherova@mail.ru>
2021-01-19 22:45:12 +0300

    Added a second task

--- FirstConsole/Program.cs
+++ FirstConsole/Program.cs
@@ -12,6 +12,16 @@ namespace firstConsole
                 Console.Write("b = "); double b = Convert.ToDouble(Console.ReadLine());
                 Console.WriteLine($"{Math.Sqrt(a * b)}");
             }
+            {
+                Console.WriteLine("Задание 2");
+                Console.Write("a = "); double a = Convert.ToDouble(Console.ReadLine());
+                Console.Write("b = "); double b = Convert.ToDouble(Console.ReadLine());
+                Console.Write("c = "); double c = Convert.ToDouble(Console.ReadLine());
+                double ac = Math.Abs(c - a), bc = Math.Abs(c - b);
+                Console.WriteLine($"AC = {ac}, BC = {bc}");
+                Console.WriteLine($"{ac} + {bc} = {ac + bc}");
+                
+            }
         }
     }
 }
